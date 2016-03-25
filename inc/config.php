f3f1cbb546c83c7d61da0c88220de6e2e346f35d
TheGabeMan <TheGabeMan@gmail.com>
2016-03-25 20:10:09 +0100

    Anonymised the /inc/config.php file

--- inc/config.php
+++ inc/config.php
@@ -1,11 +1,11 @@
 <?php
 
 $config = array('db_ip' => '127.0.0.1', 
-		'db_user' => 'nest_admin',
-		'db_pass' => 'N35tP@55',
+		'db_user' => '<dbusername>',
+		'db_pass' => '<dbuserpassword>',
 		'db_name' => 'nest',
-		'nest_user' => 'thegabeman@gmail.com',
-		'nest_pass' => 'N35tG@b',
+		'nest_user' => '<nest email address>',
+		'nest_pass' => '<nest password>',
 		'local_tz' => 'Europe/Amsterdam' // see http://php.net/manual/en/timezones.php
 		);
 
