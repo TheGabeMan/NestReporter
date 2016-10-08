11f3bd85f82bc0a5a67c088cfbb2bf6d6aa6c21f
Gabrie van Zanten <thegabeman@gmail.com>
2016-10-08 18:14:12 +0200

    Update open-database.php

--- inc/open-database.php
+++ inc/open-database.php
@@ -1,17 +1,17 @@
 <?php
 
-# Making this DockerCompatible.
-# db_ip is the name of the docker container in which MySQL runs
-# Insert Nest
-# Third
+       # Making this DockerCompatible.
+       # db_hostname is the name of the docker container in which MySQL runs
+ 
+    // ----------- Database connection ----------//
+    $mysql_db_hostname = "GabeThermDBv001";
+    $mysql_db_user = "root";
+    $mysql_db_password = "L@mpMyG@B";
+    $mysql_db_database = "nest";  
+    $con = mysqli_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password) 
+                       or die("Failed to connect to MySQL: " . mysqli_connect_error());
+    mysqli_select_db($con, $mysql_db_database) or die("Could not select database" . mysqli_connect_error());
+   
 
-$config = array('db_ip' => 'GabeThermDBv001',
-       		'db_user' => 'root',
-       		'db_pass' => 'L@mpMyG@B',
-       		'db_name' => 'nest',
-       		'nest_user' => 'thegabeman@gmail.com',
-       		'nest_pass' => 'N35tG@b',
-       		'local_tz' => 'Europe/Amsterdam' // see http://php.net/manual/en/timezones.php
-       		);
 
 ?>
