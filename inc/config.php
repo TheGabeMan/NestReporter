defccdb5c666fc5efb0c5363d2dc4fca9d8bbc19
Gabrie van Zanten <thegabeman@gmail.com>
2016-10-08 23:08:45 +0200

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -4,8 +4,8 @@ $config = array('db_ip' => 'GabeThermDB',
 		'db_user' => 'root',
 		'db_pass' => 'L@mpMyG@B',
 		'db_name' => 'nest',
-		'nest_user' => 'thegabeman@gmail.com',
-		'nest_pass' => 'N35tG@b',
+		# 'nest_user' => 'thegabeman@gmail.com',
+		# 'nest_pass' => 'N35tG@b',
 		'local_tz' => 'Europe/Amsterdam' // see http://php.net/manual/en/timezones.php
 		);
 
